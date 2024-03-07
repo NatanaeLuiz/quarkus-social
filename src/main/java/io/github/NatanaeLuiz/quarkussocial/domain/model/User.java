951fecd14a0f05492d1f3e435397bfceadea0795
Natanael Rolim <natanael.10@hotmail.com>
2024-03-07 00:46:32 -0300

    config modal / postgresql com hibernate

---
+++ src/main/java/io/github/NatanaeLuiz/quarkussocial/domain/model/User.java
@@ -0,0 +1,39 @@
+package io.github.NatanaeLuiz.quarkussocial.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/io/github/NatanaeLuiz/quarkussocial/ret/UserResource.java
+++ src/main/java/io/github/NatanaeLuiz/quarkussocial/ret/UserResource.java
@@ -1,16 +1,23 @@
 package io.github.NatanaeLuiz.quarkussocial.ret;
 
 import io.github.NatanaeLuiz.quarkussocial.ret.dto.CreateUserRequest;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser(CreateUserRequest userRequest){
 
+        return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response listarAllUsers(){
         return Response.ok().build();
     }
 }
