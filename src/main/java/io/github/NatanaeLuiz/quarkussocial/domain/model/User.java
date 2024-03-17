4d5e91f2205d27a8ba5f6c710ac25a52880b5e7b
Natanael Rolim <natanael.10@hotmail.com>
2024-03-17 19:39:26 -0300

    FIX: Add dependencia lombok

--- src/main/java/io/github/NatanaeLuiz/quarkussocial/domain/model/User.java
+++ src/main/java/io/github/NatanaeLuiz/quarkussocial/domain/model/User.java
@@ -3,9 +3,11 @@ package io.github.NatanaeLuiz.quarkussocial.domain.model;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User { //PanacheEntity equivalente ao JPA
 
     @Id
@@ -15,27 +17,4 @@ public class User { //PanacheEntity equivalente ao JPA
     private String name;
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
--- src/main/java/io/github/NatanaeLuiz/quarkussocial/rest/dto/ResponseError.java
+++ src/main/java/io/github/NatanaeLuiz/quarkussocial/rest/dto/ResponseError.java
@@ -2,12 +2,14 @@ package io.github.NatanaeLuiz.quarkussocial.rest.dto;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.ws.rs.core.Response;
+import lombok.Data;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Data
 public class ResponseError {
 
     public static final int UNPROCESSABLE_ENTITY_STATUS = 422;
@@ -32,22 +34,6 @@ public class ResponseError {
         var responseError = new ResponseError(message, erros);
 
         return responseError;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Collection<FieldError> getErros() {
-        return erros;
-    }
-
-    public void setErros(Collection<FieldError> erros) {
-        this.erros = erros;
     }
 
     public Response withStatusCode(int code) {
