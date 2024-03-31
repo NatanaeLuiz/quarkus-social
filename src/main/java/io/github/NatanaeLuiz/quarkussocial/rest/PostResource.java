465d32ce5b88c9a7440779746d0653916c559769
Natanael Rolim <natanael.10@hotmail.com>
2024-03-31 17:43:47 -0300

    FIX: Restringindo acesso aos posts dos usuarios apenas para seguidores

--- src/main/java/io/github/NatanaeLuiz/quarkussocial/rest/PostResource.java
+++ src/main/java/io/github/NatanaeLuiz/quarkussocial/rest/PostResource.java
@@ -2,6 +2,7 @@ package io.github.NatanaeLuiz.quarkussocial.rest;
 
 import io.github.NatanaeLuiz.quarkussocial.domain.model.Post;
 import io.github.NatanaeLuiz.quarkussocial.domain.model.User;
+import io.github.NatanaeLuiz.quarkussocial.domain.repository.FollowerRepository;
 import io.github.NatanaeLuiz.quarkussocial.domain.repository.PostRepository;
 import io.github.NatanaeLuiz.quarkussocial.domain.repository.UserRepository;
 import io.github.NatanaeLuiz.quarkussocial.rest.dto.CreatePostRequest;
@@ -24,11 +25,13 @@ public class PostResource {
 
     private final UserRepository userRepository;
     private final PostRepository repository;
+    private final FollowerRepository followerRepository;
 
     @Inject
-    public PostResource(UserRepository userRepository, PostRepository repository) {
+    public PostResource(UserRepository userRepository, PostRepository repository, FollowerRepository followerRepository) {
         this.userRepository = userRepository;
         this.repository = repository;
+        this.followerRepository = followerRepository;
     }
 
     @POST
@@ -51,12 +54,36 @@ public class PostResource {
     }
 
     @GET
-    public Response listost(@PathParam("userId") Long userId){
+    public Response listost(@PathParam("userId") Long userId,
+                            @HeaderParam("followerId") Long followerId){
 
         User user = userRepository.findById(userId);
 
         if (user == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        if (followerId == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Necessario informar o Header 'followerId'")
+                    .build();
+        }
+
+        //Busca o usuario
+        User follower = userRepository.findById(followerId);
+
+        if (follower == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Não foi encontrado o seguidor 'followerId': " + followerId)
+                    .build();
+        }
+
+        //Verifica se o seguidor já segue o usuario
+        boolean follows = followerRepository.follows(follower, user);
+        if (!follows){
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("Você não tem permissão para ver esses posts")
+                    .build();
         }
 
         PanacheQuery<Post> query = repository.find(
